416362f4472abca4ff2b5a9210f1774b972e783a
Karishma271 <karishmapatel273@gmail.com>
2024-05-24 21:46:11 -0400

    add

--- Week-3/index.php
+++ Week-3/index.php
@@ -7,8 +7,32 @@
 </head>
 <body>
     <?php
-    $hour = rand(1,24);
-    echo $hour;
+        $hour = rand(1, 24);
+        echo $hour . '<br>';
+
+        if ($hour <= 6) {
+            echo "Good Morning!";
+        } elseif ($hour > 6 && $hour <= 12) {
+            echo "Good Afternoon!";
+        } elseif ($hour > 12 && $hour <= 18) {
+            echo "Good Evening!";
+        } else {
+            echo "Good Night!";
+        }
+    ?>
+
+    <h1>Magic Number</h1>
+    <?php
+        $number = rand(1, 100);  
+        if ($number % 3 == 0 && $number % 5 == 0) {
+            echo "FizzBuzz";
+        } elseif ($number % 3 == 0) {
+            echo "Fizz";
+        } elseif ($number % 5 == 0) {
+            echo "Buzz";
+        } else {
+            echo "The magic number is " . $number;
+        }
     ?>
 </body>
 </html>
