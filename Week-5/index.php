976d12d92e655d7f656fad50329815124babb370
Karishma Patel <92707635+Karishma271@users.noreply.github.com>
2024-06-07 23:32:38 -0400

    Update

--- Week-5/index.php
+++ Week-5/index.php
@@ -4,8 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>mysql</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </head>
 <body>
+<div class="container">
+        <div class="p-3 mb-2 bg-primary text-white">
+            <h2 class="text-center">Color Codes</h2>
+        </div>
+    <div class="row">
+
     <?php
     $connect = mysqli_connect('localhost', 'root', '', 'http5225');
 
@@ -21,16 +30,21 @@
         exit;
     } else{
         $row = mysqli_num_rows($result);
-        echo "<table>";
-        foreach($result as $results)
-        {               
-        $name=$results['Name'];
-        $hex=$results['Hex'];
-        echo "<tr align='center'>";
-        echo "<th>$name</th>";
-        echo "<th>$hex</th>";
+        foreach($result as $row) {
+            
+            echo '<div class="col-md-4 p-3">
+                    <div class="card my-3">
+                        <div class="card-header">
+                            <h5 class="card-title text-center font-weight-bold">'. $row['Name'] . ' </h5>
+                        </div>
+                        <div style="background-color:'. $row['Hex'] .';" class="card-body">
+                            <p class="card-text font-weight-bold">'. $row['Hex'] . '</p>
+                        </div>
+                    </div>
+                </div>';
         }
-        echo "</table>";
     } ?>
+</div>
+    </div>
 </body>
 </html>
